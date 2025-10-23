1de5d069a86cfa66fbe3446a2942abc85f2fef3a
marcosandoval <m.sandoval.3@estudiantec.cr>
2025-10-22 18:28:40 -0600

    Proyecto iniciado

---
+++ ZombieTec/src/main/java/com/mycompany/zombietec/ZombieTec.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zombietec;
+
+/**
+ *
+ * @author sando
+ */
+public class ZombieTec {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
